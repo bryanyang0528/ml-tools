d4b736ead7e2908c6b772daf72838056f37dfca9
bryan.yang <bryan.yang@ringcentral.com>
2016-08-02 02:02:01 +0000

    add test for params

--- mltools/test/test_fit.py
+++ mltools/test/test_fit.py
@@ -2,15 +2,19 @@ import nose
 from nose.tools import assert_equal, assert_true
 
 import pandas as pd
-import numpy as np
-
-from scipy import stats
-from numpy import linspace
 from mltools import fit 
 
 df = pd.read_csv('data/fit.csv', sep='\t')
 
 
 def test_get_cv():
-    cv = fit.get_cv(df, 'Weibull')
-    print(cv)
+    cv1 = fit.get_cv(df, 'Weibull')
+    cv2 = fit.get_cv(df, 'weibull')
+    cv3 = fit.get_cv(df, 'wei')
+    assert_equal(cv1, 1000)
+    assert_equal(cv2, 1000)
+    assert_equal(cv3, 1000)
+
+def test_get_cv_with_params():
+    cv1 = fit.get_cv(df, 'Weibull', cp =0.8)
+    assert_equal(cv1, 900)
